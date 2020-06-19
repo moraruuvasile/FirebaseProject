87309e25feac362de5cebaf755ee21246d1ade4c
Vasile <vaseoa@yahoo.com>
2020-06-19 18:53:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebaseproject/LoginRegisterActivity.java
@@ -0,0 +1,18 @@
+package com.example.firebaseproject;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+    }
+
+    public void loginRegister_Btn(View view) {
+    }
+}
